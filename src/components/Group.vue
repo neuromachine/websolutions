55df568574a2c0425d434a26496b331ed2ace5a8
Nero <neuromacine@gmail.com>
2025-06-27 18:57:05 +0700

    new Item component for SERVICES

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, watch } from "vue";
 import catClass from "@/components/blocks/services/catClass.vue";
+import item from "@/components/blocks/services/presentation/item.vue"
 import { useDataStore } from '@/stores/dataStore';
 const dataStore = useDataStore();
 import {useRoute} from "vue-router";
@@ -20,14 +21,13 @@ watch(
 </script>
 
 <template>
-  <!-- Start Services Section -->
   <section class="services-section section-padding">
 
     <div v-if="dataStore.isCatReady" class="container">
 
       <div class="row">
-        <div class="name">{{dataStore.category.name}}</div>
-        <div class="descr">{{dataStore.category.description}}</div>
+<!--        <div class="name">{{dataStore.category.name}}</div>-->
+<!--        <div class="descr">{{dataStore.category.description}}</div>-->
         <div class="content" v-html="dataStore.category.content"></div>
       </div>
 
@@ -43,24 +43,17 @@ watch(
       </div>
 
       <div v-if="dataStore.isHaveItems" class="row">
-        <div v-for="item in dataStore.category.blocks[0].items" class="col-lg-4 col-md-6">
-          <div class="single-services-item">
-            <div class="services-icon">
-              <i class="flaticon-development"></i>
-            </div>
-            <h3>{{item.name}}</h3>
-            <p v-for="(value, key) in item.properties">{{key}} - {{value}}</p>
-            <div class="services-btn">
-              <RouterLink class="read-more" :to="{ path: '/blocks/item/' + item.key }"><i class="bi bi-arrow-right-short"></i> Подробнее</RouterLink>
-            </div>
-          </div>
-        </div>
+        <item v-for="item in dataStore.category.blocks[0].items"
+              :slug="item.key"
+              :name="item.name"
+              :properties="item.properties"
+        />
       </div>
 
     </div>
     <div v-else class="container"><div class="row row_load">Loading Category</div></div>
   </section>
-  <!-- End Services Section -->
+
 </template>
 
 <style scoped>
---
+++ src/components/blocks/services/presentation/item.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  slug: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  properties: {
+    type: Object,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <div class="col-lg-4 col-md-6">
+    <div class="single-services-item">
+      <div class="services-icon">
+        <i class="flaticon-development"></i>
+      </div>
+      <h3>{{props.name}}</h3>
+<!--      <div>{{props.properties}}</div>-->
+      <p v-for="(value, key) in props.properties">{{key}} - {{value}}</p>
+      <div class="services-btn">
+        <RouterLink class="read-more" :to="{ path: '/blocks/item/' + props.key }"><i class="bi bi-arrow-right-short"></i> Подробнее</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
