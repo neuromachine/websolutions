9eb59de7a4f89bcccf1e371aacf7afdd708c4898
Nero <neuromacine@gmail.com>
2025-09-22 15:17:35 +0700

    fix

--- src/components/forms/DynamicForm.vue
+++ src/components/forms/DynamicForm.vue
@@ -1,3 +1,37 @@
+<script setup>
+import { reactive } from 'vue'
+import { useFormStore } from '@/stores/formStore'
+import BaseInput from './BaseInput.vue'
+import BaseTextarea from './BaseTextarea.vue'
+import BaseSelect from './BaseSelect.vue'
+import BaseCheckbox from './BaseCheckbox.vue'
+
+const props = defineProps({
+  schema: { type: Object, required: true },
+})
+
+const store = useFormStore()
+
+const formData = reactive({})
+props.schema.fields.forEach(f => formData[f.name] = '')
+
+function resolveComponent(type) {
+  switch (type) {
+    case 'text': return BaseInput
+    case 'textarea': return BaseTextarea
+    case 'select': return BaseSelect
+    case 'checkbox': return BaseCheckbox
+    default: return BaseInput
+  }
+}
+
+async function onSubmit() {
+  await store.submitForm(props.schema.key, { ...formData })
+}
+</script>
+
+
+
 <template>
   <form @submit.prevent="onSubmit">
     <component
@@ -11,42 +45,3 @@
     <button type="submit">Отправить</button>
   </form>
 </template>
-
-<script setup>
-import {reactive} from 'vue'
-import {useFormStore} from '@/stores/formStore'
-import BaseInput from './BaseInput.vue'
-import BaseTextarea from './BaseTextarea.vue'
-import BaseSelect from './BaseSelect.vue'
-import BaseCheckbox from './BaseCheckbox.vue'
-
-const props = defineProps({
-  schema: {type: Object, required: true},
-})
-
-const store = useFormStore()
-
-// реактивные данные формы
-const formData = reactive({})
-props.schema.fields.forEach(f => formData[f.name] = '')
-
-// сопоставление type → компонент
-function resolveComponent(type) {
-  switch (type) {
-    case 'text':
-      return BaseInput
-    case 'textarea':
-      return BaseTextarea
-    case 'select':
-      return BaseSelect
-    case 'checkbox':
-      return BaseCheckbox
-    default:
-      return BaseInput
-  }
-}
-
-async function onSubmit() {
-  await store.submitForm(props.schema, {...formData})
-}
-</script>
