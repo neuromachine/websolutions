85645d23aec4da574e374027f206a78906b577a9
Nero <neuromacine@gmail.com>
2026-07-20 19:24:07 +0700

    VIBE: FE-015

--- src/components/blocks/compred/presentation/includes.vue
+++ src/components/blocks/compred/presentation/includes.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import SectionHeader from "@/components/blocks/general/ui/SectionHeader.vue";
 import Card from "@/components/blocks/general/ui/card.vue";
 import IconOffer from "@/components/blocks/services/micro/icon_offer.vue";
+import { getCardGridClasses } from "@/components/blocks/general/ui/cardGridClasses.js";
 
 const { t } = useI18n()
 
@@ -11,6 +13,13 @@ const props = defineProps({
     type: Array,
     required: true
   }
+})
+
+const colsClass = computed(() => {
+  if (!Array.isArray(props.items) || props.items.length === 0) {
+    return 'row-cols-1'
+  }
+  return getCardGridClasses(props.items.length)
 })
 </script>
 
@@ -21,8 +30,8 @@ const props = defineProps({
         <template #subtitle>{{ t('cp.includes.title') }}</template>
       </SectionHeader>
       
-      <div class="row align-items-stretch">
-        <div class="col-lg-3 col-md-6 d-flex" v-for="item in props.items" :key="item.text">
+      <div class="row align-items-stretch" :class="colsClass">
+        <div class="d-flex col" v-for="item in props.items" :key="item.text">
           <Card class="mb-4">
             <template #icon>
               <IconOffer :index="item.index" :properties="item" />
